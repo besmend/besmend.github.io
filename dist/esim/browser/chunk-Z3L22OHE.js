8b65ae21b36e49cbc38685204075c92741aecb37
Besmend Islami <besmend@gmail.com>
2023-12-11 00:09:13 +0100

    Testing Github pages

---
+++ dist/esim/browser/chunk-Z3L22OHE.js
@@ -0,0 +1,1 @@
+import{cb as r,da as o,db as i,pb as a,vb as s}from"./chunk-GAMRBFJV.js";var m=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-products"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,c){n&1&&(r(0,"p"),a(1,"products works!"),i())}});let t=e;return t})();export{m as ProductsComponent};
